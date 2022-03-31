9372d91237afa658093e0a4e74c2308428caf29c
Joseph  C <98540913+Jpena33@users.noreply.github.com>
2022-03-31 00:54:33 +0000

    Added Part A and Part B information.

--- README.md
+++ README.md
@@ -8,18 +8,21 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+The program runs an 3n+1 function that terminates once it reaches 1. Part A uses a seq3np1 function that keeps track of and returns the count, asks the user for an upper bound for the range value, verifies if the user value is positive (ends program if it isn't) and uses the range inclusively. Part B uses a drawing function that takes the number of iterations (upper bound) as a parameter, correctly graphs the results, updates world coordinates using the setworldcoordinates() based on the largest value returned from seq3np1 and prints the max so far. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 2
 
-Grace days remaining: < # >/5
+Grace days remaining: 3/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+The maximum so far only prints near the marked coordinates instead of on the upper left of the graph.   
 
 ## REFERENCES:
- < List any outside resources used >
+https://runestone.academy/ns/books/published/thinkcspy/MoreAboutIteration/The3n1Sequence.html
+https://www.w3schools.com/python/python_classes.asp
+https://runestone.academy/ns/books/published/fopp/Iteration/WPKeepingTrackofYourIteratorVariableYourIterable.html
+https://docs.python.org/3.7/library/turtle.html#turtle.setworldcoordinates
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+Nothing of importance to note. 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 
 def seq3np1(n):
     """
@@ -5,14 +6,58 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
+        #print(n)
+        count +=1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+    #print(n)                  # the last print is 1
+    return(count)
 
+def drawLineGraph(graph=None, x_start=0, y_start=0, x_end=0, y_end=0):
+   graph=turtle.Turtle()
+   graph.up()
+   graph.goto(x_start,y_start)
+   graph.down()
+   graph.goto(x_end,y_end)
+
+def setUpLineGraph(myscreen=None, graph=None, num_iterations=0, y=0):
+  writer=turtle.Turtle()
+  max_so_far = 0
+  for i in range(1,num_iterations+1):
+     result=seq3np1(i)
+     if result>max_so_far:
+        max_so_far = result
+     writer.clear()
+     writer.goto(i,result)
+     label = str(("Maximum so far: ", i, max_so_far))
+     writer.write(label)
+     myscreen.setworldcoordinates(0,0,i+10, max_so_far+10)
+     
 def main():
-	seq3np1(3)
+  userinput = int(input("Please enter upper bound value: "))
+  print(userinput)
+  if userinput <= 0:
+    print("Invalid input")
+    exit()
+    
+  print("Valid input")
+
+  window = turtle.Screen()
+  graph = turtle.Turtle()
+  window.setworldcoordinates(0,0,10,10)
+  drawLineGraph(graph, 0, -10, 0, 20)
+  drawLineGraph(graph, -10, 0, 10, 0)
+  
+  for start in range(1,userinput+1):
+    counter = seq3np1(start)
+    print("Start is:",start)
+    print("Counter is:",counter)
+    setUpLineGraph(window, graph, start)
+    
+  turtle.done()  
+    
 main()
